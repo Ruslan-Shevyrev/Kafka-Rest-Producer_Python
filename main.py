9487a97c81fba49b821affffdd48cc9838b21bbe
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2024-06-06 12:18:27 +0300

    add errors

--- main.py
+++ main.py
@@ -2,22 +2,15 @@ import uvicorn
 import json
 import os
 from fastapi import BackgroundTasks, FastAPI, Body
+from fastapi.responses import JSONResponse
 from kafka import KafkaProducer
 
-# KAFKA_BOOTSTRAP_SERVER = 'oraapex-draft:9092'
-KAFKA_BOOTSTRAP_SERVER = os.environ["KAFKA_BOOTSTRAP_SERVER"]
+KAFKA_BOOTSTRAP_SERVER = 'oraapex-draft:9092'
+# KAFKA_BOOTSTRAP_SERVER = os.environ["KAFKA_BOOTSTRAP_SERVER"]
 
 app = FastAPI()
 
 producer = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVER)
-
-
-def put_message(body_json):
-
-    producer.send(topic=body_json.get('topic'),
-                  key=bytes(body_json.get('key'), 'utf-8'),
-                  value=bytes(body_json.get('value'), 'utf-8'))
-    producer.flush()
 
 
 @app.get("/")
@@ -26,9 +19,16 @@ async def root():
 
 
 @app.post("/put_message")
-async def message(background_tasks: BackgroundTasks, data=Body()):
-    json_message = json.loads(data.decode('utf-8'))
-    background_tasks.add_task(put_message, json_message)
-    return {"success"}
+async def message(data=Body()):
+    try:
+        json_message = json.loads(data.decode('utf-8'))
+        producer.send(topic=json_message.get('topic'),
+                      key=bytes(json_message.get('key'), 'utf-8'),
+                      value=bytes(json_message.get('value'), 'utf-8'))
+        producer.flush()
+        return {"success"}
+    except Exception as e:
+        return JSONResponse(status_code=501,
+                            content={"error": str(e)})
 
 uvicorn.run(app, host="127.0.0.1", port=8000)
