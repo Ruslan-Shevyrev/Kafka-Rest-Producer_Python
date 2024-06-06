1654bac9aa1f5310820a844759750bff00be0f6a
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2024-06-06 10:02:46 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import uvicorn
+import json
+import os
+from fastapi import BackgroundTasks, FastAPI, Body
+from kafka import KafkaProducer
+
+KAFKA_BOOTSTRAP_SERVERS = os.environ["KAFKA_BOOTSTRAP_SERVERS"]
+
+app = FastAPI()
+
+producer = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS)
+
+
+def put_message(body_json):
+
+    producer.send(topic=body_json.get('topic'),
+                  key=bytes(body_json.get('key'), 'utf-8'),
+                  value=bytes(body_json.get('value'), 'utf-8'))
+    producer.flush()
+
+
+@app.get("/")
+async def root():
+    return {"service available"}
+
+
+@app.post("/put_message")
+async def message(background_tasks: BackgroundTasks, data=Body()):
+    json_message = json.loads(data.decode('utf-8'))
+    background_tasks.add_task(put_message, json_message)
+    return {"success"}
+
+uvicorn.run(app, host="127.0.0.1", port=8000)
