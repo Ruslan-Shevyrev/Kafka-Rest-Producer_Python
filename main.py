8bc424bc669c3ccc35563905451f91f0e73a68c6
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2024-06-06 13:37:39 +0300

    add errors

--- main.py
+++ main.py
@@ -29,13 +29,29 @@ async def root():
 async def message(data=Body()):
     try:
         json_message = json.loads(data.decode('utf-8'))
-        producer.send(topic=json_message.get('topic'),
-                      key=bytes(json_message.get('key'), 'utf-8'),
-                      value=bytes(json_message.get('value'), 'utf-8'))
+        topic = json_message.get('topic')
+
+        key = json_message.get('key')
+
+        if key is not None:
+            if type(key) == int:
+                key = str(key)
+            key = bytes(key, 'utf-8')
+
+        value = json_message.get('value')
+
+        if type(value) == int:
+            value = str(value)
+        value = bytes(value, 'utf-8')
+
+        producer.send(topic=topic,
+                      key=key,
+                      value=value)
         producer.flush()
         return {"success"}
     except Exception as e:
         return JSONResponse(status_code=501,
                             content={"error": str(e)})
+
 
 uvicorn.run(app, host=UVICORN_HOST, port=8000)
